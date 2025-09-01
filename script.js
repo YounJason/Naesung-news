df26144f7795c80fbcd851098dcbb217dc15af8d
Youn Jason <58214922+YounJason@users.noreply.github.com>
2025-09-01 10:45:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ load_articles().then(data => {
     init();
 });
 
-const cardHTML = ({ title, desc, img, date, slug, author}) => `
+const cardHTML = ({ title, desc, img, committedAt, slug, author}) => `
     <article class="card" onclick="location.href='v/${slug}'" style="cursor: pointer;">
         ${img ? `<img class="thumb" alt="" src="${img}">` : ""}
         <div class="content" role="group" aria-label="${title}">
             <h2 class="title">${title}</h2>
             <p class="desc">${desc}</p>
-            <div class="meta">${date} · ${author} 기자</div>
+            <div class="meta">${author} 기자 · ${committedAt}</div>
         </div>
     </article>
 `;
@@ -48,6 +48,7 @@ const load_meal = async () => {
             document.querySelector("#dinner").innerHTML = data.mealServiceDietInfo[1].row[1].DDISH_NM;
     }
 };
+
 
 
 load_meal();
